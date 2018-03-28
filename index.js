27e3c205bc4ca7815bb3b5f4adff56c911e117e3
Matias Alejo Garcia <ematiu@gmail.com>
2018-03-28 20:53:25 -0300

    sync to blockdozer

--- index.js
+++ index.js
@@ -54,7 +54,20 @@ function getTransaction(txid, node, cb) {
 }
 
 function sendTransaction(tx, node, cb) {
-  rpc(node, 'sendrawtransaction', [tx], cb);
+  request.post({
+    url: 'https://blockdozer.com/insight-api/tx/send',
+    body: {
+      rawtx: tx,
+    },
+    json:true,
+    strictSSL: false
+  }, function (err, response, body) {
+    if (body && body.error) {
+      callback(body.error);
+    } else {
+      callback(err, (body && body.result) || body);
+    }
+  });
 }
 
 function syncTransaction(txid, sourceNode, targetNode, cb) {
@@ -62,47 +75,6 @@ function syncTransaction(txid, sourceNode, targetNode, cb) {
     if (err) return cb(err);
     sendTransaction(tx, targetNode, cb);
   });
-}
-
-function syncNode(node, unionPool, progressBar, cb) {
-  var missingInputs = [];
-  if(!node.failures) node.failures = [];
-  async.eachSeries(node.missing, function(txid, done) {
-    syncTransaction(txid, unionPool.sources[txid], node, function(err) {
-      if (err && (err.message === 'Missing inputs')) {
-        missingInputs.push(txid);
-      } else if (err) {
-        progressBar && progressBar.tick();
-        if (err.message != 'transaction already in block chain') {
-          node.failures.push([txid, err.message]);
-        }
-      } else {
-        progressBar && progressBar.tick();
-      };
-      done();
-    });
-  }, function(err) {
-    cb(err, missingInputs);
-  });
-}
-
-/**
- * Sync the given node in multiple passes to deal with transactions
- * that have missing inputs.  Keep track of the count of missing 
- * transactions during each pass and if a pass fails to reduce the
- * number of missing transactions, stop.
- */
-function syncNodeMultiPass(node, unionPool, progressBar, cb) {
-  var lastMissingCount = node.missing.length + 1;
-  async.whilst(function() {
-    return node.missing.length < lastMissingCount;
-  }, function(done) {
-    lastMissingCount = node.missing.length;
-    syncNode(node, unionPool, progressBar, function(err, missingInputs) {
-      if (node.missing = missingInputs);
-      done(err);
-    });
-  }, cb);
 }
 
 /**
@@ -122,16 +94,6 @@ function getUnionPool(nodes) {
 };
 
 /**
- * Compute the set of transactions missing for each node
- * relative to the union set.
- */
-function getDiffs(nodes, unionPool) {
-  nodes.forEach(function(node) {
-    node.missing = _.difference(unionPool.items, node.mempool);
-  });
-};
-
-/**
  * Perform the actual sync.
  */
 fetchMempools(nodes, function(err) {
@@ -140,23 +102,21 @@ fetchMempools(nodes, function(err) {
     return;
   }
   var unionPool = getUnionPool(nodes);
-  getDiffs(nodes, unionPool);
-  async.eachOfSeries(nodes, function(node, index, nodeDone) {
-    if (index) console.log(); // emit a blank line between each node
-    console.log('Syncing', node.name || node.host, '(size: '+node.mempool.length+', missing: '+node.missing.length+'):');
-    var progressBar = new ProgressBar(':bar (:current of :total)', { total: node.missing.length, width: 10 });
-    syncNodeMultiPass(node, unionPool, progressBar, function(err) {
-      if (err) return nodeDone(err); 
-      progressBar.tick(node.missing.length);
-      node.failures.forEach(function(ea) {
-        console.log(ea[0]+': '+ea[1]);
-      });
-      node.missing.forEach(function(ea) {
-        console.log(ea+': missing inputs');
-      });
-      nodeDone();
+  console.log('Syncing with blockdozer (size: '+unionPool.items.length + ')';
+
+  var failures = [];
+  async.eachSeries(unionPool.items, function(txid, done) {
+    syncTransaction(txid, unionPool.sources[txid], function(err) {
+      if (err) {
+        console.log(err);
+        failures.push([txid, err.message]);
+      } else {
+        console.log(txid + ' done!');
+      };
+      done();
     });
   }, function(err) {
-    if (err) console.log('error:', err);
+    console.log(failures);
+    nodeDone();
   });
 });
